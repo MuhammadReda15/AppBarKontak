0955c2dde464ff9044ab21ddc0e076b4e15d7a60
MuhammadReda15 <12050117512@students.uin-suska.ac.id>
2022-04-10 23:33:57 +0700

    Initial commit

---
+++ app/src/main/java/com/redha/thirdtry/MainActivity.java
@@ -0,0 +1,16 @@
+package com.redha.thirdtry;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts2);
+    }
+}
